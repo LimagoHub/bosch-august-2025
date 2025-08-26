c98f2570d9da4af40cb642174104e5ff3eb440ac
Jo Wagner <jowagner@limago.de>
2025-08-26 07:59:33 +0200

    initial commit

---
+++ Tag1_01Zahlenraten/Tag1_01Zahlenraten/Tag1_01Zahlenraten.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    int zufallszahl = 623;
+    int versuch;
+
+    do {
+        std::cout << "Bitte geben Sie eine Zahl zwischen 1 und 1000 ein: " << std::endl;
+        std::cin >> versuch;
+
+        if(versuch < zufallszahl) 
+            std::cout << "Ihr Versuch ist zu klein " << std::endl;
+
+        if (versuch > zufallszahl)
+            std::cout << "Ihr Versuch ist zu gross " << std::endl;
+
+    } while (versuch != zufallszahl);
+    std::cout << "Sie haben die Zahl geraten" << std::endl;
+}
+
