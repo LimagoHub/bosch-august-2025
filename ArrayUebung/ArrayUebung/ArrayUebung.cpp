29e3a7f6e3b06ac4c49375471a6b67040559edbf
Jo Wagner <jowagner@limago.de>
2025-08-28 09:20:00 +0200

    initial commit

--- ArrayUebung/ArrayUebung/ArrayUebung.cpp
+++ ArrayUebung/ArrayUebung/ArrayUebung.cpp
@@ -6,9 +6,15 @@
 int main()
 {
     int feld[] = {10,2,90,3,13,1};
-
+    int size = sizeof(feld) / sizeof(int);
     
+    int max = feld[0];
 
-    std::cout << "Hello World!\n";
+    for (size_t i = 1; i < size; i++)
+    {
+        if (feld[i] > max) max = feld[i];
+    }
+
+    std::cout << max << std::endl ;
 }
 
