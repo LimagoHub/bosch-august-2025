2e92198d41d86f5323262785b6c55f4ff5272d0d
Jo Wagner <jowagner@limago.de>
2025-08-28 09:22:49 +0200

    initial commit

--- PlanetenUebung/PlanetenUebung/PlanetenUebung.cpp
+++ PlanetenUebung/PlanetenUebung/PlanetenUebung.cpp
@@ -1,20 +1,38 @@
-// PlanetenUebung.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
-
 #include <iostream>
+#include <string>
+
+struct Planet {
+    std::string name;
+    double masse;
+    double durchmesser;
+};
+
+class Sonnensystem {
+private:
+    Planet planeten[9];
+public:
+    Sonnensystem() {
+        for (int i = 0; i < 9; i++) {
+            planeten[i].name = "Unbekannt";
+            planeten[i].masse = -1;
+            planeten[i].durchmesser = -1;
+        }
+    }
+};
 
 int main()
 {
+    
+ 
+    Planet planeten[] = { 
+        { "Merkur", 1000.0, 500.0 },
+        { "Venus", 2000.0, 1500.0 },
+        { "Erde", 2000.0, 1500.0 } ,
+        { "Mars", 1800.0, 1500.0 } 
+    };
+
+    Sonnensystem sol;
+    sol.ausgabe();
+
     std::cout << "Hello World!\n";
 }
-
-// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
-
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
